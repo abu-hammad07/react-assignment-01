8bae2701d33a810081b62f890355b2c67de8267d
abu-hammad07 <hammadkamal2003@gmail.com>
2025-09-01 22:32:53 +0500

    completed assignment 1

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,11 +1,9 @@
 import ContactForm from "../components/ContactForm"
-import Hero from "../components/Hero"
 import NewsLetter from "../components/NewsLetter"
 
 const Contact = () => {
     return (
         <>
-            {/* <Hero /> */}
             <ContactForm />
             <NewsLetter />
         </>
